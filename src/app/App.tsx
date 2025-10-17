b39ae2eb933bc958b15a4ee3636045240a5ebd62
JDLSantos21 <jdlsantos21@hotmail.com>
2025-10-17 11:51:33 -0400

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1 className="text-red-500">Welcome to Tauri + React</h1>
+    </main>
+  );
+}
+
+export default App;
